929d58382392bf059ef8e929e2ea24a3242e3e20
sirikwan <k.sirikwann@gmail.com>
2016-04-22 13:58:04 +0700

    icon

---
+++ app/src/main/java/ice_pbru/khamhor/sirikwan/electronic22042559/Splash.java
@@ -0,0 +1,29 @@
+package ice_pbru.khamhor.sirikwan.electronic22042559;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this,MainActivity.class));
+                finish();
+
+
+            }
+        },500);
+
+
+
+    }
+}
